ad354bd7277569433516e9f1195026ff49e0ec61
Ahmed Abo-Raya <88713925+AhmedAboRaya@users.noreply.github.com>
2025-04-16 00:45:19 +0200

    feat: create Gallary component to integrate RollingGallery

---
+++ src/components/Gallary.jsx
@@ -0,0 +1,12 @@
+import RollingGallery from "@/blocks/Components/RollingGallery/RollingGallery"
+
+const Gallary = () => {
+    return (
+        <div>
+<RollingGallery />
+
+        </div>
+    )
+}
+
+export default Gallary
