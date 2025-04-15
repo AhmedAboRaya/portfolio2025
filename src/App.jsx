e46a13b89a0e880aba03de1248f8645a723f2b97
Ahmed Abo-Raya <88713925+AhmedAboRaya@users.noreply.github.com>
2025-04-16 00:44:35 +0200

    feat: add new assets and implement Orb component with WebGL effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import "./App.css";
 import SplashCursor from "./blocks/Animations/SplashCursor/SplashCursor";
-import TextPressure from "./blocks/TextAnimations/TextPressure/TextPressure";
 import About from "./components/About";
+import Gallary from "./components/Gallary";
 import Home from "./components/Home";
+import { Ripple } from "./components/magicui/ripple";
+import Project from "./components/Projects";
+import Skills from "./components/Skills";
+import Separator from "./components/ui/Separator";
 
 function App() {
   return (
-    <SplashCursor>
-      <div className="relative w-full min-h-screen">
+    <>
+      <SplashCursor />
+      <main className="min-h-screen">
+        {/* Your content components */}
         <Home />
-      </div>
-      <About />
-      <div className="h-screen text-white">w</div>
-    </SplashCursor>
+        <Ripple />
+
+        <About />
+        {/* <Separator /> */}
+        <Skills />
+        {/* <Gallary /> */}
+        <Project />
+      </main>
+    </>
   );
 }
 
