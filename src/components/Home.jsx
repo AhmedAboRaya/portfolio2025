e2a83e362bbd8d1a7c13ceb37e768afe358fccb5
Ahmed Abo-Raya <88713925+AhmedAboRaya@users.noreply.github.com>
2025-04-17 20:12:42 +0200

    refactor: clean up Home component by organizing imports and improving motion div formatting

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,36 +3,39 @@ import Header from "./Header";
 import Image from "./Image";
 import ContactButton from "./ui/ContactButton";
 import { Spotlight } from "./ui/spotlight-new";
-import { motion, useInView } from "framer-motion";
-
+import { motion } from "framer-motion";
+import { ScrollProgress } from "@/components/magicui/scroll-progress";
 const Home = () => {
   return (
     <div className="">
       <section className="flex flex-col min-h-screen relative justify-end md:justify-center items-center pb-5 md:px-8 md:pt-20 lg:px-20  mt-16 overflow-hidden">
+        <ScrollProgress />
         <Header />
         <div className="fixed top-0 left-0 w-full">
           <Spotlight />
         </div>
         <motion.div
-                  initial={{ opacity: 0 }}
-                  whileInView={{ opacity: 1 }}
-                  transition={{ duration: 0.7, ease: "easeOut" }}
-                  // viewport={{ once: false }}
-                  // exit={{ opacity: 0 }}
-                  >
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.7, ease: "easeOut" }}
+          // viewport={{ once: false }}
+          // exit={{ opacity: 0 }}
+        >
           <Image />
         </motion.div>
-        
+
         <div className="flex justify-center md:justify-between w-full">
           <ContactButton />
         </div>
       </section>
-      <motion.div className="hidden md:block md:-mt-96 relative z-[-1]"
-                initial={{ opacity: 0, y: 50 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, ease: "easeOut" }}
-                viewport={{ once: false }}
-                exit={{ opacity: 0, y: 50 }} >
+      <motion.div
+        className="hidden md:block md:-mt-96 relative z-[-1]"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, ease: "easeOut" }}
+        viewport={{ once: false }}
+        exit={{ opacity: 0, y: 50 }}
+      >
         <TextPressure />
       </motion.div>
     </div>
