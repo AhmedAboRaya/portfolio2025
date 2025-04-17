93b9b2871427aa29d452e6f9ac6e096e445e151e
Ahmed Abo-Raya <88713925+AhmedAboRaya@users.noreply.github.com>
2025-04-17 20:13:21 +0200

    feat: enhance ThreeDCardDemo with TextAnimate for titles and subtitles, and add tech icons with rotation effect

--- src/components/ui/ThreeDCardDemo.jsx
+++ src/components/ui/ThreeDCardDemo.jsx
@@ -1,29 +1,45 @@
 "use client";
 
+import { Pointer } from "../magicui/pointer";
+import { TextAnimate } from "../magicui/text-animate";
 import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
+import {motion} from "framer-motion"
 
-export function ThreeDCardDemo({ title, disc, img, subTitle, tryLink, link }) {
+export function ThreeDCardDemo({
+  title,
+  disc,
+  img,
+  subTitle,
+  tryLink,
+  link,
+  tech,
+}) {
   return (
-    <CardContainer className="inter-var z-[1999]">
+    <CardContainer className="inter-var z-[1999] ">
       <CardBody className="bg-[#0B0B0B]/60 relative group/card px-5 dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] w-auto sm:w-[30rem] h-auto rounded-xl p-6">
         <div className="absolute top-0 left-0 w-8 h-8 border-t-4 border-l-4 border-[#4BDE80] rounded-tl-lg"></div>
         <div className="absolute top-0 right-0 w-8 h-8 border-t-4 border-r-4 border-[#4BDE80] rounded-tr-lg"></div>
         <div className="absolute bottom-0 left-0 w-8 h-8 border-b-4 border-l-4 border-[#4BDE80] rounded-bl-lg"></div>
         <div className="absolute bottom-0 right-0 w-8 h-8 border-b-4 border-r-4 border-[#4BDE80] rounded-br-lg"></div>
         <div className="absolute -top-5 left-1/2 font-medium transform tracking-widest flex justify-center items-center -translate-x-1/2 bg-[#203B2A] text-[#4BDE80] border border-[#4BDE80] text-[20px] h-10 px-6 rounded-full z-20">
-          {title || "Make things float in air"}
+        <TextAnimate animation="scaleUp" by="text">
+          {title || "Make things float in air"}</TextAnimate>
         </div>
+        
         <CardItem className="text-xl  mt-4 text-white text-center w-full ">
-          {subTitle || "Make things float in air"}
+          <TextAnimate animation="scaleUp" by="text">
+          {subTitle || "Make things float in air"}</TextAnimate>
         </CardItem>
         <CardItem
           as="p"
           className=" text-lg  mt-2 text-white font-light text-center w-full "
         >
+          <TextAnimate animation="scaleUp" by="text">
           {disc ||
-            "Hover over this card to unleash the power of CSS perspective"}
+            "Hover over this card to unleash the power of CSS perspective"}</TextAnimate>
         </CardItem>
-        <CardItem className="w-full mt-4">
+        <CardItem className="w-full mt-4 ">
+        <TextAnimate animation="scaleUp" by="text">
           <img
             src={
               img ||
@@ -31,9 +47,35 @@ export function ThreeDCardDemo({ title, disc, img, subTitle, tryLink, link }) {
             }
             className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
             alt="thumbnail"
-          />
+          /></TextAnimate>
         </CardItem>
-        <div className="flex justify-between items-center mt-20">
+        <div className="flex justify-center items-center mt-7">
+          {tech.map((item, index) => {
+            const randomSpeed =
+              (Math.random() > 0.5 ? 1 : -1) * (20 + Math.random() * 40);
+            const randomStart = Math.floor(Math.random() * 360);
+
+            return (
+              <span
+                key={index}
+                className="border border-[#4BDE80] rounded-full p-2 bg-black hover:scale-110 -ml-3 group duration-300"
+                style={{
+                  transform: `rotate(${randomStart}deg)`,
+                  animation: `spin 5s linear infinite`,
+                  animationDirection: randomSpeed > 0 ? "normal" : "reverse",
+                }}
+              >
+                <img
+                  src={item}
+                  alt="tech"
+                  className="size-6 transition-transform duration-300 group-hover:scale-105 group-hover:skew-x-12 group-hover:skew-y-6"
+                />
+              </span>
+              
+            );
+          })}
+        </div>
+        <div className="flex justify-between items-center mt-5">
           <CardItem
             translateZ={20}
             translateX={-40}
@@ -41,7 +83,14 @@ export function ThreeDCardDemo({ title, disc, img, subTitle, tryLink, link }) {
             className="px-4 tracking-wide rounded-xl h-14 flex justify-center font-normal text-white hover:text-[#4BDE80] duration-300"
           >
             {link && (
-              <a href={link} target="_blank" rel="noopener noreferrer" className="text-[17px]">Watch Demo →</a>
+              <a
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-[17px]"
+              >
+                Watch Demo →
+              </a>
             )}
           </CardItem>
           {tryLink && (
@@ -51,7 +100,12 @@ export function ThreeDCardDemo({ title, disc, img, subTitle, tryLink, link }) {
               as="button"
               className="px-4 py-2 rounded-xl text-white flex justify-center items-center tracking-wider text-xs font-medium cursor-pointer border border-[#4BDE80] bg-transparent hover:bg-[#4BDE80] hover:text-black duration-300"
             >
-              <a href={tryLink} className="text-[18px]" target="_blank" rel="noopener noreferrer">
+              <a
+                href={tryLink}
+                className="text-[18px]"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Try now{" "}
               </a>
             </CardItem>
@@ -61,3 +115,4 @@ export function ThreeDCardDemo({ title, disc, img, subTitle, tryLink, link }) {
     </CardContainer>
   );
 }
+
