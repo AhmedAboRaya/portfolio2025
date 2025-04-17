1574ab28ec0631adf8c0c3889beca8716d2af9e6
Ahmed Abo-Raya <88713925+AhmedAboRaya@users.noreply.github.com>
2025-04-17 20:13:00 +0200

    feat: replace TextGenerateEffect with TextAnimate for enhanced text animation

--- src/components/ui/TextGenerateEffectDemo.jsx
+++ src/components/ui/TextGenerateEffectDemo.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { TextAnimate } from "../magicui/text-animate";
 import { TextGenerateEffect } from "./text-generate-effect";
 
 const words = `Hey, I'm Ahmed! I'm a Full-Stack Developer with a passion for building
@@ -17,5 +18,22 @@ const words = `Hey, I'm Ahmed! I'm a Full-Stack Developer with a passion for bui
 `;
 
 export function TextGenerateEffectDemo() {
-  return <TextGenerateEffect words={words} />;
+  // return <TextGenerateEffect words={words} />;
+  return(
+    <TextAnimate  animation="slideUp" by="word">
+      Hey, I'm Ahmed! I'm a Full-Stack Developer with a passion for building
+        web applications that are as smooth as they are powerful. Whether it's
+        crafting beautiful, responsive interfaces or designing scalable back-end
+        systems, I love turning ideas into reality with clean, efficient code.
+        I've worked on educational platforms, project management tools, and
+        business solutions, always striving to create seamless user experiences.
+        My tech stack includes Next.js, React, Tailwind CSS, JavaScript,
+        TypeScript, Node.js, Express.js, and MongoDB, but I'm always exploring
+        new tools to stay ahead. Beyond coding, I thrive in collaborative
+        environments where ideas flow freely, challenges fuel innovation, and
+        every project is an opportunity to grow. If you're looking for a
+        developer who's passionate, driven, and always ready to tackle the
+        next big thing—let's connect!
+    </TextAnimate>
+  )
 }
