6a903ab0771c4d52326604b2e64168d416385b73
Ahmed Abo-Raya <88713925+AhmedAboRaya@users.noreply.github.com>
2025-04-18 04:02:19 +0200

    feat: add PreLoader component for enhanced user experience during loading

---
+++ src/components/ui/pre-loader.jsx
@@ -0,0 +1,19 @@
+const PreLoader = () => {
+    return (
+<div className="ui-abstergo">
+  <div className="abstergo-loader">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+  <div className="ui-text">
+    Ahmed Abo-Raya's Portfolio
+    <div className="ui-dot"></div>
+    <div className="ui-dot"></div>
+    <div className="ui-dot"></div>
+  </div>
+</div>
+    )
+}
+
+export default PreLoader
