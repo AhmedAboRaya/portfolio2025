88c208a997659298a918fe383fea50f92b7124f6
Ahmed Abo-Raya <88713925+AhmedAboRaya@users.noreply.github.com>
2025-04-19 05:36:45 +0200

    feat: add skills data for technology stack with icons and proficiency percentages

---
+++ src/data/skills-data.js
@@ -0,0 +1,125 @@
+export const skillsData = [
+    {
+      name: "React",
+      icon: "/icons/reactIcon.png",
+      color: "#61DAFB",
+      percentage: 100,
+    },
+    {
+      name: "Next.js",
+      icon: "/icons/nextIcon.png",
+      color: "#FFFFFF",
+      percentage: 70,
+    },
+    {
+      name: "Node.js",
+      icon: "/icons/nodeIcon.png",
+      color: "#339933",
+      percentage: 80,
+    },
+    {
+      name: "Tailwind",
+      icon: "/icons/tailwindIcon.png",
+      color: "#20c8be",
+      percentage: 80,
+    },
+    {
+      name: "Framer Motion",
+      icon: "/icons/framer-motion.png",
+      color: "#59539d",
+      percentage: 80,
+    },
+    {
+      name: "C++",
+      icon: "/icons/cppIcon.png",
+      color: "#5e96cf",
+      percentage: 90,
+    },
+    {
+      name: "HTML",
+      icon: "/icons/htmlIcon.png",
+      color: "#E34F26",
+      percentage: 100,
+    },
+    {
+        name: "CSS",
+        icon: "/icons/cssIcon.png",
+        color: "#1473b7",
+        percentage: 100,
+      },
+    {
+      name: "Git",
+      icon: "/icons/gitIcon.png",
+      color: "#f05133",
+      percentage: 100,
+    },
+    {
+      name: "GitHub",
+      icon: "/icons/githupIcon.png",
+      color: "#9B30FF",
+      percentage: 100,
+    },
+    {
+      name: "MongoDB",
+      icon: "/icons/mongoIcon.png",
+      color: "#53ab43",
+      percentage: 100,
+    },
+    {
+      name: "MySQL",
+      icon: "/icons/mysqlIcon.png",
+      color: "#00BFFF",
+      percentage: 80,
+    },
+    {
+        name: "Postman",
+        icon: "/icons/postmanIcon.png",
+        color: "#fe6c38",
+        percentage: 100,
+      },
+    {
+      name: "TypeScript",
+      icon: "/icons/tsIcon.png",
+      color: "#3178C6",
+      percentage: 90,
+    },
+    {
+      name: "Express",
+      icon: "/icons/expressIcon.png",
+      color: "#FFFFFF",
+      percentage: 90,
+    },
+    {
+      name: "Figma",
+      icon: "/icons/figma.png",
+      color: "#b586d0",
+      percentage: 90,
+    },
+
+
+    {
+        name: "APIs",
+        icon: "/icons/apisIcon.png",
+        color: "#0595d4",
+        percentage: 90,
+      },
+      {
+        name: "JavaScript",
+        icon: "/icons/jsIcon.png",
+        color: "#ffc928",
+        percentage: 90,
+      },
+      {
+        name: "Redux",
+        icon: "/icons/reduxIcon.png",
+        color: "#764abd",
+        percentage: 70,
+      },
+      {
+        name: "SASS",
+        icon: "/icons/sassIcon.png",
+        color: "#c76195",
+        percentage: 60,
+      },
+  ]
+  
