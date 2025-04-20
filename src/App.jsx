59ba54359f32b8212e604ce070ac239faa5f7669
Ahmed Abo-Raya <88713925+AhmedAboRaya@users.noreply.github.com>
2025-04-20 02:15:57 +0200

    feat: enhance App component with loading state and footer integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,48 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import SplashCursor from "./blocks/Animations/SplashCursor/SplashCursor";
 import About from "./components/About";
 import Home from "./components/Home";
 import Project from "./components/Projects";
 import Features from "./components/Features";
-import SpaceAnimation from "./components/SpaceAnimation";
-import { TechStack } from "./components/TeckStack";
 import PreLoader from "./components/ui/pre-loader";
 import SkillsPage from "./components/Skills";
 import Particles from "./blocks/Backgrounds/Particles/Particles";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isExiting, setIsExiting] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsExiting(true);
+      setTimeout(() => setIsLoading(false), 1000); // Matches curtain animation duration
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
+      {isLoading && <PreLoader isExiting={isExiting} />}
+      
       <SplashCursor />
-      {/* <SpaceAnimation /> */}
-      <Particles />
-      <Home />
-      <PreLoader />
-      <About />
-      <Features />
-      <Project />
-      <SkillsPage />
-      <Contact />
-      {/* <TechStack /> */}
+      
+      
+      {!isLoading && (
+        <>
+        <Particles />
+          <Home />
+          <About />
+          <Features />
+          <Project />
+          <SkillsPage />
+          <Contact />
+          {/* <Footer /> */}
+        </>
+      )}
     </>
   );
 }
