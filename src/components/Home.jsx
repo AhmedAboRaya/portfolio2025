0d2c110975f74b238fe9617646f83903346ec297
Ahmed Abo-Raya <88713925+AhmedAboRaya@users.noreply.github.com>
2025-04-20 02:16:07 +0200

    feat: wrap Home component in MotionWrapper for enhanced animation effects

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,10 @@ import { Spotlight } from "./ui/spotlight-new";
 import { motion } from "framer-motion";
 import { ScrollProgress } from "@/components/magicui/scroll-progress";
 import { NavbarDemo } from "./ui/NavDemo";
+import MotionWrapper from "./ui/MotionWrapper";
 const Home = () => {
   return (
-    <div className="" id="home">
+    <MotionWrapper triggerOnce={true} delay={0.2} className="" id="home">
       <section className="flex flex-col min-h-screen relative justify-end md:justify-center items-center pb-5 md:px-8 md:pt-20 lg:px-20  mt-16 overflow-hidden">
         <ScrollProgress />
         <div className="fixed top-0 left-0 w-full md:px-10 z-50">
@@ -38,7 +39,7 @@ const Home = () => {
       >
         <TextPressure />
       </motion.div>
-    </div>
+    </MotionWrapper>
   );
 };
 
